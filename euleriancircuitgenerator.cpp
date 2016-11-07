6be1469bc23d6482528dc053feea23eccf3aa8c3
Sergio Salinas <sergiosalfer@gmail.com>
2016-11-07 20:19:05 -0300

    in english

--- euleriancircuitgenerator.cpp
+++ euleriancircuitgenerator.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 
+
 int main(int argc, char const *argv[]) {
-  int orden = atoi(argv[1]);
+  int order = atoi(argv[1]);
 
   //Esto crea la matrix
-  int **m;
-  m = new int* [orden];//Se hace un arreglo con la cantidad de filas que ingreso el usuario
-  for(int c = 0; c < orden; c++) {
-      m[c] = new int[orden]; //Crea un arreglo nuevo en cada espacio de memoria del arreglo anterior, columnas
-      for(int r = 0; r <orden; r++)
-        m[c][r] = 0;
+  int **matrix;
+  matrix = new int* [order];
+  for(int c = 0; c < order; c++) {
+      matrix[c] = new int[order];
+      for(int r = 0; r <order; r++)
+        matrix[c][r] = 0;
 
   }
 
@@ -24,9 +25,9 @@ int main(int argc, char const *argv[]) {
 
 
   //Libera los arreglos dentro del arreglo principal.
-  for (int i = 0; i < orden; i++){
-      delete[] m[i];
+  for (int i = 0; i < order; i++){
+      delete[] matrix[i];
   }
-  delete [] m;// Libera el arreglo principal
+  delete [] matrix;// Libera el arreglo principal
   return 0;
 }
