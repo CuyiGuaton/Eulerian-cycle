3bf1d431040781dd546fef1bd44f68cc1198a494
Sergio Salinas <sergiosalfer@gmail.com>
2016-11-07 20:32:12 -0300

    agregada funciones principales

--- functions.cpp
+++ functions.cpp
@@ -2,38 +2,59 @@
 #include <iostream>
 using namespace std;
 
+void mostrar(int **m, int orden);
+void incluir(int a, int b, int **matrix, int order);
+void eliminar(int a, int b, int **matrix, int order);
+int pertenece(int a, int b, int **matrix, int order);
+
 void mostrar(int **m, int orden){
   for (int i = 0; i < orden; i++) {
       for (int j = 0; j < orden; j++) {
-          cout << m[i][j] << endl;
+          cout << m[i][j] << " ";
       }
       cout << endl;
   }
+  cout << endl;
 }
 
+void incluir(int a, int b, int **matrix, int order){
+  matrix[a][b]= 1;
+  matrix[b][a]=1;
+}
+
+void eliminar(int a, int b, int **matrix, int order){
+  matrix[a][b]= 0;
+  matrix[b][a]= 0;
+}
+
+int pertenece(int a, int b, int **matrix, int order){
+  if(matrix[a][b] == 1)
+    return 1;
+  else
+    return 0;
+}
 
 int main(int argc, char const *argv[]) {
-  int orden = atoi(argv[1]);
+  int order = atoi(argv[1]);
 
   //Esto crea la matrix
-  int **m;
-  m = new int* [orden];//Se hace un arreglo con la cantidad de filas que ingreso el usuario
-  for(int c = 0; c < orden; c++) {
-      m[c] = new int[orden]; //Crea un arreglo nuevo en cada espacio de memoria del arreglo anterior, columnas
-      for(int r = 0; r <orden; r++)
-        m[c][r] = 0;
-
+  int **matrix;
+  matrix = new int* [order];
+  for(int c = 0; c < order; c++) {
+      matrix[c] = new int[order];
+      for(int r = 0; r <order; r++)
+        matrix[c][r] = 0;
   }
 
-
-  mostrar(m,orden);
-
+  incluir(2,3,matrix,order);
+  mostrar(matrix,order);
+  cout<<pertenece(2,3,matrix,order)<<endl;
 
 
   //Libera los arreglos dentro del arreglo principal.
-  for (int i = 0; i < orden; i++){
-      delete[] m[i];
+  for (int i = 0; i < order; i++){
+      delete[] matrix[i];
   }
-  delete [] m;// Libera el arreglo principal
+  delete [] matrix;// Libera el arreglo principal
   return 0;
 }
