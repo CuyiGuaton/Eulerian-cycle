a747d9cff607fd40a32ea795fa5ba7bc161b00c4
Sergio Salinas <sergiosalfer@gmail.com>
2016-11-16 02:32:50 -0300

    verifiaca si el grafo esta vacio

--- eulerianPath.c
+++ eulerianPath.c
@@ -1,4 +1,6 @@
+int isEmpty(int **graph, int order);
 void findEulerianRecursive(int **graph, int n, int row,int col);
+
 int indexPath=0;
 int *path;
 
@@ -11,8 +13,9 @@ void eulerianPath(int **matrix,int order) {
 void findEulerianRecursive(int **graph, int n, int row,int col){
   //printf("%d \n", row);
   mostrar(graph,n);
-
-  if(graph[row][col]==1){
+  if( isEmpty(graph,n) == 1)
+    mostrar(graph,n);
+  else if(graph[row][col]==1){
     graph[row][col]=0;
     graph[col][row]=0;
     path[indexPath]=col;
@@ -21,4 +24,12 @@ void findEulerianRecursive(int **graph, int n, int row,int col){
   }
   else
     findEulerianRecursive(graph, n, row, col+1);
+}
+
+int isEmpty(int **graph, int order){
+  for (size_t row = 0; row < order; row++)
+    for (size_t col = 0; col < order; col++)
+      if (graph[row][col] == 1 )
+        return 0;
+    return 1;
 }
